634ae2efb66d8948334f18e2db947836e5fafaff
SolomiyaShchebak <79625823+SolomiyaShchebak@users.noreply.github.com>
2021-03-03 20:51:42 +0200

    Lab 1-3

---
+++ Conditions.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main1()
+{
+	/*
+	//Ternary operator
+	int a, b, c, max;
+	cout << "Enter a, b, c: " << endl;
+	cin >> a >> b >> c;
+
+	max = (a > b && a > c) ? a : (b > a && b > c) ? b : c;
+	cout << "Max is: " << max;
+	return 0;
+	*/
+
+	//Switch
+	/*
+	int n;
+	cin >> n;
+
+	switch (n)
+	{
+	case 1:
+		cout << "Mon" << endl;
+		break;
+	case 2:
+		cout << "Tue"<<endl;
+		break;
+	case 3:
+		cout << "Mon" << endl;
+		break;
+	// ...
+	case 7:
+		cout << "Sun" << endl;
+	}
+	return 0;
+	*/
+
+	//Max digit of 3-digit number
+	/*
+	int a, d1, d2, d3, max;
+	cout << "Enter a digit: " << endl;
+	cin >> a;
+
+	d3 = -a % 10;
+	d2 = (-a / 10) % 10;
+	d1 = -a / 100;
+
+	max = (d1 > d2 && d1 > d3) ? d1 : (d2 > d1 && d2 > d3) ? d2 : d3;
+	cout << "Maximum = " << max;
+
+	return 0;
+	*/
+	cout << "Hehe";
+	return 0;
+}
